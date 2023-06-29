8da0c2ebf464fc3dac30752df5727fc8d95680dd
mkudinov <m.kudinov@icloud.com>
2023-06-29 19:34:55 +0300

    homework 3

--- task_19/Program.cs
+++ task_19/Program.cs
@@ -5,11 +5,12 @@ do
     number = Convert.ToInt32(Console.ReadLine());
 } while (number > 99999 || number < 10000);
 
+
+Console.WriteLine(isPalindrome(number));
+
 bool isPalindrome(int num)
 {
     if (num / 10000 == num % 10 && num / 1000 % 10 == num % 100 / 10)
         return true;
     return false;
 }
-
-Console.WriteLine(isPalindrome(number));
