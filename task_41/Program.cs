0c093aa16ec08d47bfdb1ad4c0a0fdfe056a2a7e
mkudinov <m.kudinov@icloud.com>
2023-07-10 17:40:17 +0300

    homework 6

---
+++ task_41/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Enter quantity of entered numbers: ");
+int quantity = Convert.ToInt32(Console.ReadLine());
+
+int[] numbers = new int[quantity];
+for (int i = 0; i < quantity; i++)
+{
+    Console.Write("Enter a number: ");
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int positives = countPositive(numbers);
+Console.WriteLine(positives);
+
+int countPositive(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+            count++;
+    }
+    return count;
+}
---
+++ task_43/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine($"[{x},{y}]");
