2451a1097c7a497bd9e2aab846e07e4a06eb941f
mkudinov <m.kudinov@icloud.com>
2023-07-20 16:25:58 +0300

    additional task

---
+++ task_420/Program.cs
@@ -0,0 +1,13 @@
+﻿void nSums(int a, int b, int n)
+{
+    if(n > 0)
+    {
+        Console.Write(a + " ");
+        int temp = b;
+        b = a + b;
+        a = temp;
+        nSums(a, b, n - 1);
+    }
+}
+
+nSums(6, 10, 4);
